6c4efa3e964f10cd2366c5153f2ce8b333884dd1
ElYannu87 <y.driever@gmail.com>
2021-04-19 16:04:19 +0200

    views setup and dB adaptation

--- stockmanagement/stockmgmt/views.py
+++ stockmanagement/stockmgmt/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    title = 'Welcome to the placeholder'
+    context = {
+        "title" : title,
+    }
+    return render(request, "home.html", context)
