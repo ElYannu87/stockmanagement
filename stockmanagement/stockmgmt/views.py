4a4a087a89daad5e86a4c27fef643c18e2961631
ElYannu87 <y.driever@gmail.com>
2021-04-21 14:59:57 +0200

    added fronted and crispy forms

--- stockmanagement/stockmgmt/views.py
+++ stockmanagement/stockmgmt/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Stock
 from .form import StockCreateForm
 
 # Create your views here.
 
+
 def home(request):
     title = 'Welcome to the placeholder'
     context = {
-        "title" : title,
+        "title": title,
     }
     return render(request, "home.html", context)
 
+
 def list_item(request):
-    title = 'List of Items'
+    title = 'Stocks'
     queryset = Stock.objects.all()
     context = {
-        "title" : title,
-        "queryset" : queryset,
+        "title": title,
+        "queryset": queryset,
     }
     return render(request, "list_item.html", context)
+
 
 def add_items(request):
     form = StockCreateForm(request.POST or None)
     if form.is_valid():
         form.save()
+        return redirect('/list_item')
     context = {
         "form": form,
         "title": "Add Item",
